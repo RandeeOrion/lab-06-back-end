061dd41624b8bc3a16eb9b9ae6b9c307abc0600c
Randee Orion <randeegood@gmail.com>
2020-01-18 15:18:07 -0800

    map populates

--- README.md
+++ README.md
@@ -5,6 +5,23 @@ Estimate of time needed to complete: __25__
 
 Start time: _1:35_
 
+Finish time: __1:55___
+
+Actual time needed to complete: __20 min___
+
+
+
+Number and name of feature: __2: locations____
+
+Estimate of time needed to complete: ___1 hour_
+
+Start time: _1:55_
+
 Finish time: _____
 
-Actual time needed to complete: _____
+Actual time needed to complete: ____
+
+
+
+
+
--- server.js
+++ server.js
@@ -3,7 +3,47 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.use(express.static('./data'));
+// app.use(express.static('./data'));
+app.use(cors());
+
+// app.get('/', (request, response) => {
+//   response.send('Home Page!');
+// })
+
+
+
+app.get('/location', (request, response) => {
+  try {
+    const geoData = require('./data/geo.json');
+    const city = request.query.city;
+    console.log(request.query);
+    const locationData = new Location(city, geoData);
+    response.send(locationData);
+  }
+  catch(error){
+    errorHandler('so sorry, something went wrong.', request, response);
+  }
+});
+
+
+function Location(city, geoData){
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+}
+
+const errorHandler = (error, request, response) => {
+  response.status(500).send(error);
+}
+
+
+
+
+
+app.listen(PORT, () => console.log('server up on Port'));
+
