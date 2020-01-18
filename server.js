6275bd93401440e709a8192aadb9717fa01a1456
Randee Orion <randeegood@gmail.com>
2020-01-18 13:52:22 -0800

    last bit of stuff

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict'
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./data'));
